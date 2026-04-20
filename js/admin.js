a09d3919d3ac4820a53f6278b07af548d86c9f2e
ahmadmusthofa-ux <ahmadmusthofa@unesa.ac.id>
2026-04-20 10:19:10 +0700

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,16 +1,25 @@
 function loginAdmin(){
-  if(email.value==="admin" && password.value==="admin123"){
-    localStorage.setItem("admin","yes");
-    location.href="admin.html";
-  }
-  else{alert("Gagal");
-  }
-function logout(){
-  if(confirm("Yakin mau logout?")){
-    localStorage.removeItem("admin");
-    window.location.href = "admin-login.html";
-  }
-}
-  if(localStorage.getItem("admin")!=="yes"){
-  location.href="admin-login.html";
+
+  const user = document.getElementById("email").value;
+  const pass = document.getElementById("password").value;
+  const loader = document.getElementById("loader");
+
+  loader.style.display = "block";
+
+  setTimeout(()=>{
+
+    if(user==="admin" && pass==="admin123"){
+      localStorage.setItem("admin","yes");
+
+      loader.innerHTML = "✅ Berhasil...";
+      setTimeout(()=>{
+        window.location.href="admin.html";
+      },800);
+
+    }else{
+      loader.style.display = "none";
+      alert("Username / Password salah");
+    }
+
+  },1000);
 }
